01cde8b43521342741e2cc9171bd96cdca1faf97
Pyon18Pyon <ayasayshi@gmail.com>
2021-07-29 16:03:32 +1000

    feat: music data

---
+++ src/songs.js
@@ -0,0 +1,32 @@
+ export const songs = [
+  {
+    name: 'ba-1',
+    displayName: 'Don\'t Look Back',
+    artist: 'DJ Gollum'
+  },
+  {
+    name: 'ba-2',
+    displayName: 'Hero',
+    artist: 'Nick Skitz & Technoposse'
+  },
+  {
+    name: 'ba-3',
+    displayName: 'I Live For That Energy',
+    artist: 'Armin Van Buuren'
+  },
+  {
+    name: 'ba-4',
+    displayName: 'For You',
+    artist: 'Tough Diamond'
+  },
+  {
+    name: 'ba-5',
+    displayName: 'The Universe',
+    artist: 'Hardwell'
+  },
+  {
+    name: 'ba-6',
+    displayName: 'Rock the party',
+    artist: 'Nick Skitz & Raverockers ft.'
+  }
+];
